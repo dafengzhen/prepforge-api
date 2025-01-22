d51dfd9a0c74cdd769bfd123301519b67b77c95e
dafengzhen <dafengzhen@aliyun.com>
2025-01-22 18:11:54 +0800

    chore: update config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,16 @@ import eslint from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import perfectionist from 'eslint-plugin-perfectionist';
+import { includeIgnoreFile } from '@eslint/compat';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
 
 export default tseslint.config(
+  includeIgnoreFile(gitignorePath),
   {
     ignores: ['eslint.config.mjs'],
   },
